ddbec2d0dda8ad503f2846a5c1fc0a7ea4d8f3e0
SeoSang <ddrrpg@naver.com>
2020-02-05 00:31:03 +0900

    웹펙 세팅완료

---
+++ ReactToTS/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const webpack = require("webpack")
+var HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  name: "ReactToTS",
+  mode: "development", // 배포할 때는 production
+  devtool: "eval", // 배포할 때는 hidden-source-map
+  resolve: {
+    extensions: [".js", "jsx", ".tsx", ".ts"]
+  },
+  entry: {
+    app: ["./client"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties", "react-hot-loader/babel"]
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        options: {}
+      },
+      {
+        test: /\.css?$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/dist/"
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: "./index.html"
+  //   })
+  // ]
+}
